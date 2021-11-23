b0fbb563cb1bd090255b102a23e842ac626838fe
Antônio Campos <aclcampos@live.com>
2021-11-23 19:29:41 -0300

    Realizado requisito 7

--- src/component/ExpenseTable.js
+++ src/component/ExpenseTable.js
@@ -1,23 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class ExpenseTable extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
-        <td>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </td>
+        <thead>
+          <tr className="table">
+            <th className="table-input">Descrição</th>
+            <th className="table-input">Tag</th>
+            <th className="table-input">Método de pagamento</th>
+            <th className="table-input">Valor</th>
+            <th className="table-input">Moeda</th>
+            <th className="table-input">Câmbio utilizado</th>
+            <th className="table-input">Valor convertido</th>
+            <th className="table-input">Moeda de conversão</th>
+            <th className="table-input">Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            expenses.map((expense) => {
+              const { id, description, tag, method, value, currency } = expense;
+              const findCurrency = Object.entries(expense.exchangeRates).find(
+                (currencyActual) => expense.currency === currencyActual[0],
+              );
+              const currencyName = findCurrency[1].name.replace('/Real Brasileiro', '');
+              return (
+                <tr key={ id } className="table">
+                  <td className="table-input">{description}</td>
+                  <td className="table-input">{tag}</td>
+                  <td className="table-input">{method}</td>
+                  <td className="table-input">{value}</td>
+                  <td className="table-input">{currencyName}</td>
+                  <td className="table-input">{Number(findCurrency[1].ask).toFixed(2)}</td>
+                  <td className="table-input">{Number(findCurrency[1].ask * value).toFixed(2)}</td>
+                  <td className="table-input">Real</td>
+                  <td className="table-input">Editar/Excluir</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
       </table>
     );
   }
 }
 
-export default ExpenseTable;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses });
+
+ExpenseTable.propTypes = PropTypes.shape({
+}).isRequired;
+
+export default connect(mapStateToProps)(ExpenseTable);
